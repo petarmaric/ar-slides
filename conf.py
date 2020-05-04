c2f377c4c2f7981c2706c0213b7d99e882010e0b
Petar Marić <petar.maric@gmail.com>
2020-05-04 20:43:39 +0200

    sphinx: Added the Reveal.js HTML presentation slides Sphinx builder

--- conf.py
+++ conf.py
@@ -30,6 +30,7 @@ copyright = f'{date.today().year}, {author}'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_revealjs',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
